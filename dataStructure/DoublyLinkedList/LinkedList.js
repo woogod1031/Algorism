b7b0249372df4bb7dbcedcff9950ccc7bdbb2cea
Woohyun <ktkwhms3@gmail.com>
2022-05-16 21:27:40 +0900

    Feat: Implement Doubly Linked List

---
+++ dataStructure/DoublyLinkedList/LinkedList.js
@@ -0,0 +1,71 @@
+import Node from "./LinkedNode";
+
+//노드 출력(정/역방향)
+//노드 추가
+//특정 인덱스 노드 추가
+//노드 삭제
+//특정 인덱스 노드 삭제
+//특정 노드 인덱스 출력
+
+class DoubleyLinkedList {
+  constructor() {
+    let init = new Node("init");
+    this.head = init; //처음에 데이터가 없다면 head는 null이다.
+    this.tail = init;
+
+    this.현재노드 = undefined;
+    this.데이터수 = 0;
+  }
+
+  length() {
+    return this.데이터수;
+  }
+
+  append(data) {
+    let 새로운노드 = new Node(data);
+
+    this.tail.next = 새로운노드;
+    this.tail = 새로운노드;
+    this.데이터수 += 1;
+  }
+
+  toString() {
+    let 순회용현재노드 = this.head;
+    순회용현재노드 = 순회용현재노드.next;
+
+    let s = "";
+    for (let i = 0; i < this.데이터수; i++) {
+      s += `${순회용현재노드.data},`;
+      순회용현재노드 = 순회용현재노드.next;
+    }
+    return `[${s.slice(0, -2)}]`;
+  }
+
+  get fullData() {
+    let 순회용현재노드 = this.head;
+    순회용현재노드 = 순회용현재노드.next;
+
+    let s = "";
+    for (let i = 0; i < this.데이터수; i++) {
+      s += `${순회용현재노드.data},`;
+      순회용현재노드 = 순회용현재노드.next;
+    }
+    return JSON.parse(`[${s.slice(0, -2)}]`);
+  }
+
+  insert(index, data) {
+    let 순회용현재노드 = this.head;
+    순회용현재노드 = 순회용현재노드.next;
+
+    for (let i = 0; i < index - 1; i++) {
+      순회용현재노드 = 순회용현재노드.next;
+    }
+
+    let 새로운노드 = new Node(data);
+
+    새로운노드.next = 순회용현재노드.next;
+    순회용현재노드.next = 새로운노드;
+
+    this.데이터수 += 1;
+  }
+}
