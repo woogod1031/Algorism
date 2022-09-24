944c26bc61a1c3f0a01dbe095aa9830767f69e8f
KimWooHyun <ktkwhms3@gmail.com>
2022-09-24 23:58:10 +0900

    백준 readLine

---
+++ baekjoon/readLineSolution.js
@@ -0,0 +1,31 @@
+//한 줄 입력
+
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+let num = Number(input);
+
+for (let i = 1; i <= num; i++) {
+  console.log(i);
+}
+
+//여러 줄 입력
+
+let fs = require("fs");
+let input2 = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let count = input2[0];
+let numbers = [];
+
+for (let i = 1; i < input2.length; i++) {
+  if (input2[i] !== "") {
+    numbers.push(input2[i].split(" "));
+  }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  let num1 = Number(numbers[i][0]);
+  let num2 = Number(numbers[i][1]);
+
+  console.log(num1 + num2);
+}
