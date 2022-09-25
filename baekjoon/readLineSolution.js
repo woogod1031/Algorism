1553e7427f1a9e7a49106a398654c77c9fac9d4a
KimWooHyun <ktkwhms3@gmail.com>
2022-09-25 15:53:10 +0900

    백준 js, read file

--- baekjoon/readLineSolution.js
+++ baekjoon/readLineSolution.js
@@ -1,31 +1,40 @@
 //한 줄 입력
+import { readFileSync } from "fs";
 
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+// let input = readFileSync("예제.txt").toString().split(" ");
+// console.log("input:", input);
+// //let num = Number(input);
+// for (let i = 1; i <= input.length; i++) {
+//   console.log(i);
+// }
 
-let num = Number(input);
+// //한 줄 입력
+// let fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+// let num = Number(input);
 
-for (let i = 1; i <= num; i++) {
-  console.log(i);
-}
+// for (let i = 1; i <= num; i++) {
+//   console.log(i);
+// }
 
 //여러 줄 입력
 
-let fs = require("fs");
-let input2 = fs.readFileSync("/dev/stdin").toString().split("\n");
+//let fs = require("fs");
+// let input = readFileSync("예제.txt").toString().split("\n");
+// console.log(input);
+// let count = input[0];
+// let numbers = [];
 
-let count = input2[0];
-let numbers = [];
+// for (let i = 1; i < input.length; i++) {
+//   if (input[i] !== "") {
+//     numbers.push(input[i].split(" "));
+//   }
+// }
+// console.log("numbers:", numbers);
 
-for (let i = 1; i < input2.length; i++) {
-  if (input2[i] !== "") {
-    numbers.push(input2[i].split(" "));
-  }
-}
+// for (let i = 0; i < numbers.length; i++) {
+//   let num1 = Number(numbers[i][0]);
+//   let num2 = Number(numbers[i][1]);
 
-for (let i = 0; i < numbers.length; i++) {
-  let num1 = Number(numbers[i][0]);
-  let num2 = Number(numbers[i][1]);
-
-  console.log(num1 + num2);
-}
+//   console.log(num1 + num2);
+// }
