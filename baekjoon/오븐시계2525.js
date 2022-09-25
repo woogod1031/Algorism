ec030e4ae425096a4d8f63653381d409e208cef6
KimWooHyun <ktkwhms3@gmail.com>
2022-09-25 18:03:48 +0900

    Update

--- baekjoon/오븐시계2525.js
+++ baekjoon/오븐시계2525.js
@@ -1,22 +1,17 @@
 import fs from "fs";
 
 let input = fs.readFileSync("예제.txt").toString().split("\n");
-let now = input[0].split(" ").map((param) => Number(param));
-let need = input[1];
+let now = input[0].split(" ").map((param) => +param);
+let need = +input[1];
 
 function solution(now, need) {
-  let hour = parseInt(need / 60);
-  let min = need % 60;
-  for (let i = 0; i < hour; i++) {
-    now[0] = now[0] + 1 >= 24 ? 0 : now[0] + 1;
+  let time = [now[0], now[1] + need];
+  if (time[1] >= 60) {
+    time[0] += parseInt(time[1] / 60);
+    time[0] %= 24;
+    time[1] %= 60;
   }
-
-  now[0] =
-    now[0] + parseInt((now[1] + min) / 60) >= 24
-      ? 0
-      : now[0] + parseInt((now[1] + min) / 60);
-  now[1] = (now[1] + min) % 60;
-  console.log(now[0], now[1]);
+  console.log(time.join(" "));
 }
 
 solution(now, need);
