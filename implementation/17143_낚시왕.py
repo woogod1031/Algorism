bcbd14072f14c421976e85962f9971c86fcb5eaf
KimWooHyun <ktkwhms3@gmail.com>
2023-10-19 18:11:39 +0900

    Feat: 낚시왕 정답코드

--- implementation/17143_낚시왕.py
+++ implementation/17143_낚시왕.py
@@ -1,73 +1,64 @@
 from sys import stdin
 
-map_r, map_c, m = map(int, stdin.readline().split())
-arr = [0] * m
-total_shark_size = 0
-for i in range(m):
-  r, c, s, d, z = map(int, stdin.readline().split())
-  arr[i] = {'id':i, 'r':r, 'c':c, 's':s, 'd':d, 'z':z }
+UP, DOWN, RIGHT, LEFT = 1, 2, 3, 4
+R, C, M = map(int, input().split())
+board = [[0 for _ in range(C)] for _ in range(R)]
 
-for j in range(map_c):
-  cur_column = j + 1    
-  selcted_sharks = [n for n in arr if n['c'] == cur_column]  
-  if len(selcted_sharks):
-    target = max(selcted_sharks, key= lambda n: map_r - n['r'])        
-    arr = [x for x in arr if x['id'] != target['id']]
-    total_shark_size += target['z']
-  result = []
-  for idx in range(len(arr)):
-    target = arr[idx].copy()
-    if target['d'] == 1 or target['d'] == 2:
-      cycle = map_r * 2 - 2
-      current = 0                  
-      if target['d'] == 1:
-        current = (cycle - target['r'] + 2) + target['s']                
-      else:
-        current = target['r'] + target['s']                
-      current %= cycle
-      if current > map_r:
-        target['r'] = cycle - current + 2
-        target['d'] = 1
-      elif current == 0:
-        target['r'] = 2
-        target['d'] = 1
-      else:
-        target['r'] = current
-        target['d'] = 2      
-    else: 
-      cycle = map_c * 2 - 2
-      current = 0
-      if target['d'] == 3:
-        current = target['c'] + target['s']                
-      else:
-        current = (cycle - target['c'] + 2) + target['s']                
-      current %= cycle
-      if current > map_c:
-        target['c'] = cycle - current + 2
-        target['d'] = 4     
-      elif current == 0:
-        target['c'] = 2
-        target['d'] = 1 
-      else:
-        target['c'] = current
-        target['d'] = 3   
-           
-    if len(result):
-      for ix in range(len(result)):      
-        if result[ix]['c'] == target['c'] and result[ix]['r'] == target['r']:
-          if result[ix]['z'] > target['z']:
-            break
-          else:
-            n_result = []            
-            n_result = [v for v in result if v['id'] != result[ix]['id']]    
-            n_result.append(target)            
-            result = n_result            
-            break    
+def fish(j):
+  for i in range(R):
+    if board[i][j]:
+      x = board[i][j][2]
+      board[i][j] = 0
+      return x      
+  return 0
+
+def move():
+  global board
+  new_board = [[0 for _ in range(C)] for _ in range(R)]
+  for i in range(R):
+    for j in range(C):
+      if board[i][j]:
+        # 다음 위치를 구한다
+        ni, nj, nd = get_next_loc(i, j, board[i][j][0], board[i][j][1])
+        if new_board[ni][nj]:
+          new_board[ni][nj] = max(
+            new_board[ni][nj], 
+            (board[i][j][0], nd, board[i][j][2]),
+            key=lambda x: x[2],
+          )
         else:
-          if ix + 1 == len(result):
-            result.append(target)        
-          else: continue
+          new_board[ni][nj] = (board[i][j][0], nd, board[i][j][2])
+  board = new_board
+
+def get_next_loc(i, j, speed, dir):
+  if dir == UP or dir == DOWN:
+    cycle = R * 2 - 2
+    if dir == UP:
+      speed += 2 * (R - 1) - i
     else:
-      result.append(target)         
-  arr = result            
-print(total_shark_size)  
+      speed += i
+    speed %= cycle
+    if speed >= R:
+        return (2 * R - 2 - speed, j, UP)
+    return (speed, j, DOWN)      
+  else:
+    cycle = C * 2 - 2    
+    if dir == LEFT:
+        speed += 2 * (C - 1) - j
+    else:
+        speed += j
+    speed %= cycle
+    if speed >= C:
+        return (i, 2 * C - 2 - speed, LEFT)
+    return (i, speed, RIGHT)
+
+for i in range(M):
+    r, c, s, d, z = map(int, input().split())
+    r, c = r - 1, c - 1
+    board[r][c] = (s, d, z)
+ans = 0
+for j in range(C):
+  ans += fish(j)  
+  move()
+    
+print(ans) 
