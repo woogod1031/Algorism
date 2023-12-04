538b11b6a5f841a57726af7c484129ac68fdcee2
KimWooHyun <ktkwhms3@gmail.com>
2023-12-04 16:02:17 +0900

    Feat: dfs 기본 예제

--- implementation/이코테_게임개발.py
+++ implementation/이코테_게임개발.py
@@ -1,18 +1,35 @@
+
+#   0 1 2 3 4
+# 0 o o o o o
+# 1 o o o o o
+# 2 o o x o o
+# 3 o o o o o
+# 4 o o o o o
+
+# [[o, o, o,      o, o],
+#  [o, o, o,      o, o],
+#  [o, o, x(2,2), o, o],
+#  [o, o, o,      o, o],
+#  [o, o, o,      o, o]]
+
 dx = { 0:-1, 1:0, 2:1, 3:0,}
 dy = { 0:0, 1:1, 2:0, 3:-1,}
+# dx = [-1, 0, 1, 0]
+# dy = [0, 1, 0, -1]
 n_dir = { 0:3, 3:2, 2:1, 1:0,}
+# n_dir = [3,0,1,2]
 
 n,m = list(map(int,input().split()))
 a,b,d = list(map(int,input().split()))
 field = []
 visited = [[0 for _ in range(m)] for _ in range(n)]
+# [[0] * m for _ in range(n)]
 
 for i in range(n):  
   field.append(list(map(int,input().split())))  
 
 visited[a][b] = 1
 cnt = 1
-
 
 while True:
   ps = False
