a195256fb5e5e8874251dbd2d5da15a7de368a7e
KimWooHyun <ktkwhms3@gmail.com>
2023-12-05 19:08:30 +0900

    Refactor: bfs_촌수계산 리팩토링

--- DFS_BFS/촌수계산_2644.py
+++ DFS_BFS/촌수계산_2644.py
@@ -1,38 +1,31 @@
 from collections import deque
 
 n = int(input())
-people = [[] for _ in range(n + 1)]
-visited = [[False] for _ in range(n + 1)]
-a, b = list(map(int, input().split()))
-m = int(input())
 
+people = [[] for _ in range(n + 1)]
+checked = [[False] for _ in range(n + 1)]
+
+a, b = list(map(int, input().split()))
+
+m = int(input())
 for i in range(m):
   x, y = list(map(int, input().split()))  
   people[x].append(y)
   people[y].append(x)
 
-connenct = False
-cnt = 0
+def bfs():    
+  que = deque()
+  que.append(([a], 0))
+  while que:
+    node, cnt = que.popleft()  
+    for i in node:
+      if checked[i] == True:
+        continue
+      else:
+        if i == b:               
+          return cnt  
+        checked[i] = True
+        que.append((people[i], cnt + 1))        
+  return -1
 
-que = deque()
-que.append(([a], cnt))
-
-while que:
-  v, c = que.popleft()  
-  for i in v:
-    if visited[i] == True:
-      continue
-    else:
-      visited[i] = True
-      que.append((people[i], c + 1))
-      if i == b:
-        connenct = True   
-        cnt = c
-        break
-  if connenct == True:
-    break
-
-if connenct == True:
-  print(cnt)
-else:
-  print(-1)
+print(bfs())
