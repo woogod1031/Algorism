56286d5a061064e9adce4d1b2fecc911ac4ef495
KimWooHyun <ktkwhms3@gmail.com>
2023-12-29 14:15:58 +0900

    Feat: 최단경로 dijkstra 예제 알고리즘

---
+++ shortest/dijkstra_default.py
@@ -0,0 +1,73 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+# 노드 수(6), 간선 수(11)
+n, m = map(int, input().split())
+# 시작 노드(1)
+start = int(input())
+graph = [[] for i in range(n + 1)]
+
+# 방문 여부
+visited = [False] * (n+1)
+# 최단 거리 초기화
+distance = [INF] * (n+1)
+
+# 간선 입력
+# 1 2 2
+# 1 3 5
+# 1 4 1
+
+# 2 3 3
+# 2 4 2
+
+# 3 2 3
+# 3 6 5
+
+# 4 3 3
+# 4 5 1
+
+# 5 3 1
+# 5 6 2
+for _ in range(m):
+  # a에서 b로가는 비용 c
+  a,b,c = map(int, input().split())
+  graph[a].append((b,c))
+
+#------------------------setting------------------------#
+
+def get_smallest_node():
+  min_value = INF
+  index = 0
+  for i in range(1, n + 1):
+    if distance[i] < min_value and not visited[i]:
+      min_value = distance[i]
+      index = i
+  
+  return index
+
+def dijkstra(start):
+  distance[start] = 0
+  visited[start] = True
+  for j in graph[start]:     
+    distance[j[0]] = j[1]
+  for _ in range(n-1):
+    now = get_smallest_node()
+    visited[now] = True
+    for j in graph[now]:
+      # 만약 graph[4]일 떄: graph[4] = [(3, 3), (5, 1)]
+      # 1 => 4 => 3 값이: cost = distance[now] + j[1]
+      # 1 => 3 값보다 작으면: cost < distance[j[0]]:
+      # 3까지 가는 최단경로를 변경해준다: distance[j[0]] = cost
+      cost = distance[now] + j[1]
+      if cost < distance[j[0]]:
+        distance[j[0]] = cost
+
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+  if distance[i] == INF:
+    print("INFINITY")
+  else:
+    print(distance[i])
---
+++ shortest/dijkstra_default_heap.py
@@ -0,0 +1,59 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+# 노드 수(6), 간선 수(11)
+n, m = map(int, input().split())
+# 시작 노드(1)
+start = int(input())
+graph = [[] for i in range(n + 1)]
+# 최단 거리 초기화
+distance = [INF] * (n+1)
+
+# 간선 입력
+# 1 2 2
+# 1 3 5
+# 1 4 1
+
+# 2 3 3
+# 2 4 2
+
+# 3 2 3
+# 3 6 5
+
+# 4 3 3
+# 4 5 1
+
+# 5 3 1
+# 5 6 2
+for _ in range(m):
+  # a에서 b로가는 비용 c
+  a,b,c = map(int, input().split())
+  graph[a].append((b,c))
+
+#------------------------setting------------------------#
+def dijkstra(start):
+  queue = []
+  # heapq에서 튜플의 첫번째 원소를 기준으로 우선순위를 정한다.
+  # 따라서 최단거리를 추출하기 위해 튜플은 (거리(0), 노드(1))로 구성된다.
+  heapq.heappush(queue, (0, start))
+  distance[start] = 0 
+
+  while queue:
+    dist, now = heapq.heappop(queue)
+    if distance[now] < dist:
+      continue
+    for i in graph[now]:
+      cost = dist + i[1]
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heapq.heappush(queue, (cost, i[0])) 
+      
+dijkstra(start)
+
+for i in range(1, n + 1):
+  if distance[i] == INF:
+    print("INFINITY")
+  else:
+    print(distance[i])
