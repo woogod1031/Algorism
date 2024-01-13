c37dd15a63d608f18de761e2c8d3b3c9ddc3a528
KimWooHyun <ktkwhms3@gmail.com>
2024-01-13 23:57:12 +0900

    Feat: 특정거리도시찾기_18352 다익스트라 사용

--- DFS_BFS/특정거리도시찾기_18352.py
+++ DFS_BFS/특정거리도시찾기_18352.py
@@ -1,32 +1,38 @@
-from collections import deque
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
 n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
-# visited = [0 for _ in range(n + 1)]
+distance = [INF] * (n+1)
+start = x
 
+# 모든 비용은 1로 다 같다.
 for i in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
+    graph[a].append((b, 1))
 
-# 모든 도시에 대한 최단 거리 초기화
-distance = [-1] * (n + 1)
-distance[x] = 0  # 출발 도시까지의 거리는 0으로 설정
 
-# 너비 우선 탐색(BFS) 수행
-q = deque([x])
+def dijkstra(start_node):
+    queue = []
+    heapq.heappush(queue, (0, start_node))
+    distance[start_node] = 0
 
-while q:
-    now = q.popleft()
-    # 현재 도시에서 이동할 수 있는 모든 도시를 확인
-    for next_node in graph[now]:
-        # 아직 방문하지 않은 도시라면
-        if distance[next_node] == -1:
-            # 최단 거리 갱신
-            distance[next_node] = distance[now] + 1
-            q.append(next_node)
+    while queue:
+        cur_distance, cur_node = heapq.heappop(queue)
+        if distance[cur_node] < cur_distance:
+            continue
+        for child_node, child_distance in graph[cur_node]:
+            new_distance = cur_distance + child_distance
+            if new_distance < distance[child_node]:
+                distance[child_node] = new_distance
+                heapq.heappush(queue, (new_distance, child_node))
+
+
+dijkstra(start)
 
 answer = []
-print(distance)
 for i in range(1, n + 1):
     if distance[i] == k:
         answer.append(i)
