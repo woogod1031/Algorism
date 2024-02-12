420b04ed5f9a6701b889fb36b96f052c3e9e7e24
KimWooHyun <ktkwhms3@gmail.com>
2024-02-12 13:14:07 +0900

    Feat: 구글 못생긴수

--- dp/못생긴수.py
+++ dp/못생긴수.py
@@ -1,21 +1,16 @@
 n = int(input())
 
-ugly = [0] * n
-ugly[0] = 1
+ugly = [False] * 1001
+ugly[1] = True
 
-i2 = i3 = i5 = 0
-next2, next3, next5 = 2, 3, 5
+for i in range(2, 1001):
+    if i % 2 == 0:
+        ugly[i] = True
+    elif i % 3 == 0:
+        ugly[i] = True
+    elif i % 5 == 0:
+        ugly[i] = True
 
-for l in range(1, n):
-    ugly[l] = min(next2, next3, next5)
-    if ugly[l] == next2:
-        i2 += 1
-        next2 = ugly[i2] * 2
-    if ugly[l] == next3:
-        i3 += 1
-        next3 = ugly[i3] * 3
-    if ugly[l] == next5:
-        i5 += 1
-        next5 = ugly[i5] * 5
+result = [i for i in range(1, 1001) if ugly[i] == True]
 
-print(ugly[n - 1])
+result[n]
