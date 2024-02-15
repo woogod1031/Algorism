f8dc95ad1d581fc80f9efb9d2a5da60b71f8e211
KimWooHyun <ktkwhms3@gmail.com>
2024-02-15 15:51:47 +0900

    Feat: 무지의 먹방 라이브

--- kakao/무지의먹방라이브.py
+++ kakao/무지의먹방라이브.py
@@ -1,28 +1,29 @@
+import heapq
 # food_times 의 길이는 1 이상 200,000 이하이다.
 # food_times 의 원소는 1 이상 100,000,000 이하의 자연수이다.
 # k는 1 이상 2 x 10^13 이하의 자연수이다.
 
+
 def solution(food_times, k):
-    cnt = 0
-    while k != 0:
-        if sum(food_times) == 0:
-            return -1
-        val = cnt % len(food_times)
-        if food_times[val] != 0:
-            food_times[val] -= 1
-            cnt += 1
-            k -= 1
-        else:
-            cnt += 1
-
-    if sum(food_times) == 0:
+    if sum(food_times) <= k:
         return -1
-    for _ in range(len(food_times)):
-        target = cnt % len(food_times)
-        if food_times[target] != 0:
-            return target + 1
-        else:
-            cnt += 1
 
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i + 1))
 
-print(solution([1, 100], 10))
+    length = len(food_times)
+    total = 0
+    prev_time = 0
+
+    # 다음 차례에도 k값이 살아있어야되기 때문에 <=을 사용한다.
+    # k가 6이라면 6의 위치가 아닌 그 다음에 먹어야되는 7에 해당하는 위치를 알아야 하기 때문에
+    while (total + ((q[0][0] - prev_time) * length)) <= k:
+        now = heapq.heappop(q)[0]
+        __what = (now - prev_time) * length
+        total += __what
+        prev_time = now
+        length -= 1
+
+    result = sorted(q, key=lambda x: x[1])
+    return result[(k-total) % length][1]
