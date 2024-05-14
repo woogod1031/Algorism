a307e649a039edba5f2be3e3211e267e5082e4b2
KimWooHyun <ktkwhms3@gmail.com>
2024-05-14 15:52:24 +0900

    Feat: hackerrank 웜업 테스트

---
+++ hackerrank/MiniMaxSum.js
@@ -0,0 +1,52 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+  arr = arr.sort((a, b) => b - a);
+  let max = 0;
+  let min = 0;
+
+  arr.forEach((c, i) => {
+    if (i !== 0) {
+      min += c;
+    }
+    if (i !== 4) {
+      max += c;
+    }
+  });
+  console.log(min, max);
+}
+
+function main() {
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  miniMaxSum(arr);
+}
