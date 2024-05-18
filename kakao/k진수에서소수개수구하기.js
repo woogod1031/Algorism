6993e2d709a5b25517cfc5938121f68838179572
KimWooHyun <ktkwhms3@gmail.com>
2024-05-18 12:58:57 +0900

    Feat: k진수에서 소수개수 구하기

---
+++ kakao/k진수에서소수개수구하기.js
@@ -0,0 +1,22 @@
+function isPrime(num) {
+  if (num === 1) return false;
+
+  for (let i = 2; i < Math.sqrt(num + 1) + 1; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(n, k) {
+  n = n.toString(k).split("0");
+
+  const result = n.filter((v) => {
+    if (v == "" || !isPrime(+v)) {
+      return false;
+    }
+    return true;
+  });
+  return result.length;
+}
+
+console.log(solution(437674, 3));
