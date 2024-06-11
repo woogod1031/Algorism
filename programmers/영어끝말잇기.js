75b64ae8d41111f47d17858f7fece3a71b1936b2
KimWooHyun <ktkwhms3@gmail.com>
2024-06-11 15:29:37 +0900

    Refactor: 영어끝말잇기 set 사용해서 재풀이

--- programmers/영어끝말잇기.js
+++ programmers/영어끝말잇기.js
@@ -1,47 +1,21 @@
 function solution(n, words) {
-  const map = new Map();
-
   // 시작 세팅
-  let circul = 1;
-  let playerIndex = 0;
-  let before_word = words.shift();
-  map.set(before_word, 1);
+  const set = new Set();
+  set.add(words.at(0));
 
-  // words를 순회한다.
-  while (words.length > 0) {
-    // 한바퀴를 다 돌면 circul을 +1해준다
-    if (playerIndex + 1 == n) {
-      circul += 1;
-      playerIndex = (playerIndex + 1) % n;
-    } else {
-      playerIndex += 1;
-    }
+  // words의 개수만큼 반복한다.
+  for (let i = 1; i < words.length; i++) {
+    const current_word = words.at(i);
 
-    const current_word = words.shift();
-
-    // 이전 단어의 마지막과 현재 단어의 맨 처음을 비교
-    if (before_word.at(-1) !== current_word.at(0)) {
-      return [playerIndex + 1, circul];
-    }
-    // map에 현재 단어가 있는지 확인
-    if (map.has(current_word)) return [playerIndex + 1, circul];
-    before_word = current_word;
-    map.set(current_word, 1);
+    // map에 현재 단어가 있는지 확인 또는 이전 단어의 마지막과 현재 단어의 맨 처음을 비교
+    if (set.has(current_word) || words.at(i - 1).at(-1) !== words[i].at(0))
+      return [(i % n) + 1, Math.ceil((i + 1) / n)];
+    set.add(current_word);
   }
 
   return [0, 0];
 }
 
 console.log(
-  solution(3, [
-    "tank",
-    "kick",
-    "know",
-    "wheel",
-    "land",
-    "dream",
-    "mother",
-    "robot",
-    "tank",
-  ])
+  solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])
 );
