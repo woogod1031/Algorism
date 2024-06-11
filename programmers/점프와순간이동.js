c8e73240966c708251002b127f127a6bb80631d4
KimWooHyun <ktkwhms3@gmail.com>
2024-06-11 13:46:36 +0900

    Feat: 삼각달팽이, 점프와 순간이동 오답풀이

--- programmers/점프와순간이동.js
+++ programmers/점프와순간이동.js
@@ -1,18 +1,33 @@
 function solution(n) {
-  let result = 1;
-  let num = n;
-
-  while (num > 1) {
-    if (num % 2 == 0) {
-      num /= 2;
+  let result = 0;
+  while (n > 0) {
+    if (n % 2 == 0) {
+      n /= 2;
     } else {
-      num -= 1;
       result += 1;
+      n -= 1;
     }
   }
-
   return result;
 }
+
+console.log(solution(5));
+
+// function solution(n) {
+//   let result = 1;
+//   let num = n;
+
+//   while (num > 1) {
+//     if (num % 2 == 0) {
+//       num /= 2;
+//     } else {
+//       num -= 1;
+//       result += 1;
+//     }
+//   }
+
+//   return result;
+// }
 
 // dp 풀이법, 효율성 0점
 
